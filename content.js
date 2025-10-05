ef167519d447d10b5d8c3551482bea8096cfc177
srinjoy2075 <iamsrinjoy03@gmail.com>
2025-10-05 20:04:03 +0530

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ function getArticleText() {
   const article = document.querySelector("article");
   if (article) return article.innerText;
 
-  // fallback
+ 
   const paragraphs = Array.from(document.querySelectorAll("p"));
   return paragraphs.map((p) => p.innerText).join("\n");
 }
@@ -13,3 +13,4 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
     sendResponse({ text });
   }
 });
+
